7b9de2234ebdadfa919484940f4b0cfc07a57b30
Dave Holst <46893433+daveholst@users.noreply.github.com>
2021-07-30 16:02:09 +0800

    create user working from front-end

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -2,7 +2,7 @@ import {gql} from '@apollo/client'
 
 export const CREATE_USER = gql`
   mutation createUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
+    createUser(username: $username, email: $email, password: $password) {
       token
       user {
         _id
